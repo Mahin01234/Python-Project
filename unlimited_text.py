83f359430f8c41773688f4b1f8fcf3eed431641c
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-05-12 13:19:23 +0600

    Add files via upload

---
+++ unlimited_text.py
@@ -0,0 +1,12 @@
+import pyperclip
+import time
+import pyautogui
+
+text = "Hello "   
+time.sleep(5)           
+
+while True:
+    pyperclip.copy(text)               
+    pyautogui.hotkey("ctrl", "v")       
+    pyautogui.press("enter")            
+    time.sleep(1)                      
